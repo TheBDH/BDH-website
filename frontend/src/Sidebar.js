ce4ee4a1ad108d5c41c3f0af3df0a90ba0860d14
Rahul Jayaraman <rahulj.4999@gmail.com>
2018-10-17 22:40:08 -0400

    [5188] Added Google Ads and Link Tracking (#18)
    * 5188: Added Google DFP Ads and Link Tracking Code; integrated Blog and Post- RSS Feeds
    
    * Fixed bug in BlogDailyHerald links

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -1,36 +1,92 @@
 import React, { Component } from 'react';
 import Advertisement_300x250 from './Advertisement_300x250';
+import { DFPSlotsProvider } from 'react-dfp';
+import * as rssParser from 'react-native-rss-parser';
+
 
 class Sidebar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
+    //Below code gets content from the post- RSS feed and loads into state
+    var postUrl = 'http://post.browndailyherald.com/feed/';
+    fetch(proxyUrl + postUrl)
+     .then((response) => response.text())
+     .then((responseData) => rssParser.parse(responseData))
+     .then((rss) => {
+      let postArticles = [];
+      let postLinks = [];
+      rss.items.map(k => { postArticles.push(k.title); postLinks.push(k.id); });
+      this.setState( { postArticles, postLinks });
+    });
+
+    var blogUrl = 'http://feeds.browndailyherald.com/BlogDailyHerald';
+    fetch(proxyUrl + blogUrl)
+      .then((response) => response.text())
+      .then((responseData) => rssParser.parse(responseData))
+      .then((rss) => {
+        let blogArticles = [];
+        let blogLinks = [];
+        rss.items.map(k => { blogArticles.push(k.title); blogLinks.push(k.links[0]); });
+        this.setState( { blogArticles, blogLinks })
+      });
+  }
+
   render() {
+    //Below code creates the elements for links to post- articles
+    var postArticleLinks = [];
+    if (this.state.postArticles && this.state.postLinks) {
+      for(var i=0; i < 4; i++){
+        postArticleLinks.push(<a className='small-article-title' href={this.state.postLinks[i]}>{this.state.postArticles[i]}</a>);
+      }
+    }
+
+    var blogArticleLinks = [];
+    if (this.state.blogArticles && this.state.blogLinks) {
+      for(var i=0; i < 4; i++){
+        blogArticleLinks.push(<a className='small-article-title' href={this.state.blogLinks[i].url}>{this.state.blogArticles[i]}</a>);
+      }
+    }
+
+    console.log(this.state);
+
     return (
+      <DFPSlotsProvider dfpNetworkId='1149905'>
       <div className="sidebar">
-        <Advertisement_300x250 url={this.props.ad1.url} name={this.props.ad1.name} imgUrl={this.props.ad1.imgUrl}/>
+        <Advertisement_300x250 adUnit='BDH_RightSidebar_300x250'/>
         <div className="sidebar-post">
           <a href="http://post.browndailyherald.com/">
             <img src="/static/images/post-logo.png" alt="Post--Online" width="250"/>
           </a>
           <div className="sidebar-articles">
-            {this.props.post_articles.map(article => <a className="small-article-title" href={article.url}>{article.title}</a>)} 
+            {postArticleLinks} 
           </div>
         </div>
-        <Advertisement_300x250 url={this.props.ad1.url} name={this.props.ad1.name} imgUrl={this.props.ad1.imgUrl}/>
+        <Advertisement_300x250 adUnit='BDH_RightSidebar2_300x250'/>
         <div className="sidebar-blog">
           <a href="http://blogdailyherald.com/">
             <img src="/static/images/blog-logo.png" alt="Blog Daily Herald" width="250"/>
           </a>
           <div className="sidebar-articles">
-            {this.props.blog_articles.map(article => <a className="small-article-title" href={article.url}>{article.title}</a>)} 
+            {blogArticleLinks}
           </div>
         </div>
+        <Advertisement_300x250 adUnit='BDH_RightSidebar3_300x250' />
         <div className="sidebar-latest">
           <h3 className="med-nav-title">Latest Issue</h3>
           <a href={this.props.latest_issue.url}>
             <img src={this.props.latest_issue.imgUrl} alt={this.props.latest_issue.title}/>
           </a>
         </div>
-        <Advertisement_300x250 url={this.props.ad2.url} name={this.props.ad2.name} imgUrl={this.props.ad2.imgUrl}/>
+        <Advertisement_300x250 adUnit='BDH_RightSidebar4_300x250' />
       </div>
+      </DFPSlotsProvider>
     );
   }
 }
