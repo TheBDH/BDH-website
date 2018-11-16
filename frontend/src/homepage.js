540c336e203c5d85dc7dfb489e947f1d2ce5b564
Rahul Jayaraman <rahulj.4999@gmail.com>
2018-11-15 20:37:53 -0500

    Fawn fixes as well as other AWS stuff

--- frontend/src/homepage.js
+++ frontend/src/homepage.js
@@ -1,38 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import './general-style.css';
-import App from './App';
-import Header from './Header';
-import Sidebar from './Sidebar';
-import Index_Featured_Article_Grid from './Index_Featured_Article_Grid';
-import Index_Sections_Grid from './Index_Sections_Grid';
-import Advertisement_728x90 from './Advertisement_728x90';
-import Footer from './Footer';
-import Article from './Article'
-import registerServiceWorker from './registerServiceWorker';
-import {AdSlot, DFPManager} from 'react-dfp';
 import NonSports from './NonSports';
 import Author_Info from'./Author_Info';
+import ReactDOM from 'react-dom';
+import React from 'react';
 
-function renderArticle(){
-	var article = document.getElementById('article-component');
-	var year = article.getAttribute('year');
-	var title = article.getAttribute('title');
-	ReactDOM.render(<Article year={year} title={title}/>, document.getElementById('article-component'));
-}
-
-function renderHeader(){
-	var mainNav = [{url:'#', title:'Home'}, {url:'#', title:'News', dropList:[{url:'#', title:'Home'}, {url:'#', title:'Home'}]}, {url:'#', title:'Arts and Culture'}, {url:'#', title:'Science and Research'}, {url:'#', title:'Sports'}, {url:'#', title:'Opinion'}, {url:'http://post.browndailyherald.com', title:'POST'}, {url:'http://www.blogdailyherald.com', title:'Blog'}];
-	ReactDOM.render(<Header mainNav={mainNav}/>, document.getElementById('headerNew'));
-}
-
-function renderSidebar(){
-	var post_articles = [{title:"Lorem ipsum", url:"#"},{title:"Nulla eu risus sem", url:"#"},{title:"article 3", url:"#"},{title:"article 4", url:"#"}]
-	var blog_articles = [{title:"article 1", url:"#"},{title:"article 2", url:"#"},{title:"article 3", url:"#"},{title:"article 4", url:"#"}]
-	var latest_issue = {title:"latest issue", url:"#", imgUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/2010-brown-bear.jpg/200px-2010-brown-bear.jpg"};
-	ReactDOM.render(<Sidebar post_articles={post_articles} blog_articles={blog_articles} latest_issue={latest_issue}/>, document.getElementById('sidebar'));
-}
+import Index_Featured_Article_Grid from './Index_Featured_Article_Grid';
+import Index_Sections_Grid from './Index_Sections_Grid';
+import './general-style.css';
 
 function renderIndexFeatured(){
 	var hero = {imgUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/2010-brown-bear.jpg/200px-2010-brown-bear.jpg",imgAlt:"bear",section:{name:"University News", url:"#"},url:"#",title:"Lorem ipsum dolor",author:{name:"John Doe", url:"#"},date:"May 10, 2010",excerpt:"Lorem ipsum dolor sit amet, elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem  minim veniam, quis nostrud exerci tation ullamcorper Lorem ipsum dolor sit amet, consectetuer [...]"}
@@ -55,10 +28,12 @@ function renderIndexSections(){
 	ReactDOM.render(<Index_Sections_Grid sections={sections_list}/>, document.getElementById('section'));
 }
 
-
-function renderFooter(){
-	ReactDOM.render(<Footer/>, document.getElementById('footerNew'));
+function renderHomePage() {
+	renderIndexSections();
+	renderIndexFeatured();
 }
+
+export default { renderHomePage }
 
 // function renderNonSports(){
 // 	var nonsports1 = <NonSports sectionHeader={"University News"} title={"Medium title for home"} author={"Author Name"} date={"Oct 3 2018"}
@@ -85,18 +60,6 @@ function renderFooter(){
 //
 // 	ReactDOM.render([author], document.getElementById('authorInfo'));
 // }
-function renderAd() {
-	ReactDOM.render(<Advertisement_728x90 adUnit="BDH_ATF_Article_728x90" />, document.getElementById('728x90'));
-	ReactDOM.render(<Advertisement_728x90 adUnit="BDH_Footer_728x90" />, document.getElementById('728x90f'));
-}
 
-renderHeader();
-renderFooter();
-renderSidebar();
-//renderIndexFeatured();
-//renderIndexSections();
-renderAd();
-//renderArticle();
 //renderNonSports();
 //renderAuthorInfo();
-registerServiceWorker();
