8b18b6a5b86e339c27a1d5daa3464c490995cc34
Vellichora <fangrui_tong@brown.edu>
2018-12-06 21:03:49 -0500

    updates to header and template (#36)
    * sports section component and header modifications
    
    * Header changes and template changes

--- frontend/src/Advertisement_300x250.js
+++ frontend/src/Advertisement_300x250.js
@@ -3,12 +3,15 @@ import { AdSlot } from 'react-dfp';
 
 class Advertisement_300x250 extends Component {
   render() {
+  	var visibility = "ad-open";
     return (
-      <div className="ad_300x250 advertisement">
-      <p>ADVERTISEMENT</p>
-        <AdSlot sizes={[[300, 250]]} adUnit={this.props.adUnit}/>
+    	<div className={visibility}>
+    		<div className="ad_300x250 advertisement">
+    			<p>ADVERTISEMENT</p>
+        		<AdSlot sizes={[[300, 250]]} adUnit={this.props.adUnit}/>
+      		</div>
       </div>
-      );
+    );
   }
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './general-style.css';
+import './fixed-style.css';
 import App from './App';
 import Header from './Header';
 import Sidebar from './Sidebar';
