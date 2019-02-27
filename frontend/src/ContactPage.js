c913ba6d4c4bbf82cd5bf199ed79402b9e493d77
Alan D. Yoho <36675874+alandyoho@users.noreply.github.com>
2019-02-27 00:49:17 -0500

    static pages completed, added to react router, share links working, topics hyperlinked (#48)

---
+++ frontend/src/ContactPage.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Advertisement_728x90 from './Advertisement_728x90'
+
+
+const ContactPage = () => (
+    <div class='main-content'>
+        <Advertisement_728x90 adUnit="BDH_ATF_Article_728x90" />
+        <h1>Contact</h1>
+        <h3>Address and Office Hours</h3>
+        <p>The Brown Daily Herald, Inc.
+            <br />
+            P.O. Box 2538
+            <br />
+            Providence, RI 02906
+        </p>
+        <p>
+            Our offices are located at 195 Angell St. in Providence, R.I.<br />
+            Open Monday through Thursday, 10a.m. to 3 p.m. (Closed on Friday and weekends.)
+        </p>
+        <h3>Phone Number</h3>
+        <p>Business Phone: 401-351-3372</p>
+        <h3>Looking for Comment?</h3>
+        <p>If you’re from the media looking for a comment, do not contact any writers.
+            They are not authorized to speak on behalf of The Herald.
+            For comment, email herald@browndailyherald.com.
+        </p>
+        <h3>Contact us via email</h3>
+        <dl>
+            <dt>General editorial information and news tips:</dt>
+            <dd>herald@browndailyherald.com</dd>
+            <dt>Business:</dt>
+            <dd>gm@browndailyherald.com</dd>
+            <dt>Advertising:</dt>
+            <dd>advertising@browndailyherald.com</dd>
+            <dt>Letters to the editor:</dt>
+            <dd>letters@browndailyherald.com</dd>
+            <dt>Opinions:</dt>
+            <dd>opinions@browndailyherald.com</dd>
+            <dt>Sports:</dt>
+            <dd>sports@browndailyherald.com</dd>
+            <dt>Website:</dt>
+            <dd>web@browndailyherald.com</dd>
+            <dt>Post (Weekly Magazine):</dt>
+            <dd>post@browndailyherald.com</dd>
+        </dl>
+    </div>
+);
+
+export default ContactPage;
